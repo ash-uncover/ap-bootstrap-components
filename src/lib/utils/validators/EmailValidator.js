cfd69ced7967e6977445c983b3e67968637c4da1
aSH <ash.uncover@gmail.com>
2017-08-28 23:21:37 +0200

    fixed null props reference

--- src/lib/utils/validators/EmailValidator.js
+++ src/lib/utils/validators/EmailValidator.js
@@ -5,7 +5,6 @@ export default class EmailValidator extends ValidatorBase {
 	constructor(props) {
 		super(props)
 		this.regSuccess = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		this.msgError = props.msgError || 'Veuillez saisir une adresse électronique valide'
 	}
 
 	getState(value) {
--- src/lib/utils/validators/PhoneValidator.js
+++ src/lib/utils/validators/PhoneValidator.js
@@ -5,7 +5,6 @@ export default class PhoneValidator extends ValidatorBase {
 	constructor(props) {
 		super(props)
 		this.regSuccess = /^0[1-9]([-. ]?[0-9]{2}){4}$/
-		this.msgError = props.msgError || 'Veuillez saisir un numéro de téléphone valide'
 	}
 
 	getState(value) {

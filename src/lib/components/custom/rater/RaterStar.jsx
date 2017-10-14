65f39280391fbe2429020244c372603448c0e933
aSH <ash.uncover@gmail.com>
2017-10-14 20:45:20 +0200

    update rater star

--- src/lib/components/custom/rater/RaterStar.jsx
+++ src/lib/components/custom/rater/RaterStar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Base from 'lib/components/Base'
 
-import { Glyphicon } from 'lib/exports'
+import { Glyphicon, Button } from 'lib/exports'
 
 import './ap-rater-star.scss'
 
@@ -9,27 +9,44 @@ class RaterStar extends Base {
 
     constructor(props) {
         super(props)
-        this.baseClasses = [ 'ap-rater-star' ]
+        this.baseClasses = [ 'ap-rater' ]
         // Sub components properties
-        this.fullStarProps = { className: 'ap-rater-star-full', glyph: 'star' }
-        this.emptyStarProps = { className: 'ap-rater-star-empty', glyph: 'star-empty' }
+        this.raterProps = {}
+        this.fullStarProps = { 
+            className: 'ap-rater-star ap-rater-star-full', 
+            glyph: 'star' 
+        }
+        this.emptyStarProps = { 
+            className: 'ap-rater-star ap-rater-star-empty', 
+            glyph: 'star-empty' 
+        }
         // Component properties
         this.propsInfos = {
             required : {
             },
             optionnal : {
                 value: {},
-                starMax: 5
+                onChange: {},
+                starMax: { defaultValue: 0, store: this.raterProps }
             }
         }
     }
     
+
+    // View Callbacks //
+    // --------------------------------------------------------------------------------
+
+    onChange(value, event) {
+        this.props.onChange && this.props.onChange(event, value + 1)
+    }
+
+
     // Rendering functions //
     // --------------------------------------------------------------------------------
 
     _resolveValue() {
         let value = this.props.value || 0
-        if (value < 0 || value > this.props.starMax) {
+        if (value < 0 || this.props.starMax && value > this.props.starMax) {
             return 0
         }
         return Math.round(value)
@@ -38,7 +55,19 @@ class RaterStar extends Base {
     _buildFullStars(nb) {
         let result = []
         for (let i = 0 ; i < nb ; i ++) {
-            result.push(<Glyphicon key={'full-' + i} {...this.fullStarProps} />)
+            if (this.props.onChange) {
+                result.push(
+                    <Button
+                        className='ap-rater-button'
+                        key={'full-' + i} 
+                        bsSize='xs'
+                        onClick={this.onChange.bind(this, i)} >
+                        <Glyphicon {...this.fullStarProps} />
+                    </Button>
+                )
+            } else {
+                result.push(<Glyphicon key={'full-' + i} {...this.fullStarProps} />)
+            }
         }
         return result
     }
@@ -46,9 +75,27 @@ class RaterStar extends Base {
     _buildEmptyStars(nb) {
         let result = []
         for (let i = 0 ; i < nb ; i ++) {
-            result.push(<Glyphicon key={'empty-' + i} {...this.emptyStarProps} />)
+            if (this.props.onChange) {
+                result.push(
+                    <Button
+                        className='ap-rater-button'
+                        key={'empty-' + i} 
+                        bsSize='xs'
+                        onClick={this.onChange.bind(this, this.props.value + i)} >
+                        <Glyphicon {...this.emptyStarProps} />
+                    </Button>
+                )
+            } else {
+                result.push(<Glyphicon key={'empty-' + i} {...this.emptyStarProps} />)
+            }
         }
         return result   
+    }
+
+    _buildClasses() {
+        let classes = this.baseClasses.slice()
+        if (this.props.onChange) classes.push('ap-rater-change')
+        return classes
     }
     
     render() {
@@ -57,7 +104,7 @@ class RaterStar extends Base {
         return (
             <div className={this.className}>
                 {this._buildFullStars(value)}
-                {this._buildEmptyStars(this.props.starMax - value)}
+                {this._buildEmptyStars((this.props.starMax || 0) - value)}
             </div>
         )
     }

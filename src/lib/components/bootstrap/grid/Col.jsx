dc9e3fd7d400fc18e588cdedd24b534797b1f480
aSH <ash.uncover@gmail.com>
2017-11-11 12:16:42 +0100

    support 0 for column offset/push/pull

--- src/lib/components/bootstrap/grid/Col.jsx
+++ src/lib/components/bootstrap/grid/Col.jsx
@@ -42,20 +42,20 @@ class Col extends Base {
 		if (this.props.md) classes.push('col-md-' + this.props.md)
 		if (this.props.lg) classes.push('col-lg-' + this.props.lg)
 		// Offsets
-		if (this.props.xsOffset) classes.push('col-xs-offset-' + this.props.xsOffset)
-		if (this.props.smOffset) classes.push('col-sm-offset-' + this.props.smOffset)
-		if (this.props.mdOffset) classes.push('col-md-offset-' + this.props.mdOffset)
-		if (this.props.lgOffset) classes.push('col-lg-offset-' + this.props.lgOffset)
+		if (this.props.xsOffset || this.props.xsOffset === 0) classes.push('col-xs-offset-' + this.props.xsOffset)
+		if (this.props.smOffset || this.props.smOffset === 0) classes.push('col-sm-offset-' + this.props.smOffset)
+		if (this.props.mdOffset || this.props.mdOffset === 0) classes.push('col-md-offset-' + this.props.mdOffset)
+		if (this.props.lgOffset || this.props.lgOffset === 0) classes.push('col-lg-offset-' + this.props.lgOffset)
 		// Push
-		if (this.props.xsPush) classes.push('col-xs-push-' + this.props.xsPush)
-		if (this.props.smPush) classes.push('col-sm-push-' + this.props.smPush)
-		if (this.props.mdPush) classes.push('col-md-push-' + this.props.mdPush)
-		if (this.props.lgPush) classes.push('col-lg-push-' + this.props.lgPush)
+		if (this.props.xsPush || this.props.xsPush === 0) classes.push('col-xs-push-' + this.props.xsPush)
+		if (this.props.smPush || this.props.smPush === 0) classes.push('col-sm-push-' + this.props.smPush)
+		if (this.props.mdPush || this.props.mdPush === 0) classes.push('col-md-push-' + this.props.mdPush)
+		if (this.props.lgPush || this.props.lgPush === 0) classes.push('col-lg-push-' + this.props.lgPush)
 		// Pull
-		if (this.props.xsPull) classes.push('col-xs-pull-' + this.props.xsPull)
-		if (this.props.smPull) classes.push('col-sm-pull-' + this.props.smPull)
-		if (this.props.mdPull) classes.push('col-md-pull-' + this.props.mdPull)
-		if (this.props.lgPull) classes.push('col-lg-pull-' + this.props.lgPull)
+		if (this.props.xsPull || this.props.xsPull === 0) classes.push('col-xs-pull-' + this.props.xsPull)
+		if (this.props.smPull || this.props.smPull === 0) classes.push('col-sm-pull-' + this.props.smPull)
+		if (this.props.mdPull || this.props.mdPull === 0) classes.push('col-md-pull-' + this.props.mdPull)
+		if (this.props.lgPull || this.props.lgPull === 0) classes.push('col-lg-pull-' + this.props.lgPull)
 		return classes
 	}
 	
